6d75d8c08273f122a0752d2969adbacb336b2050
Josh Malkinson <joshua.malkinson@coop.co.uk>
2023-12-01 21:42:52 +0000

    Automate making files for each new day

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # README
+
+Solutions to [https://adventofcode.com/2023](https://adventofcode.com/2023).
+
+## File Generation
+
+To make a new directory called `day-05` containing `solution.ts`, `solution.test.ts` (with some boilerplate) and `input.txt`, run
+
+```sh
+DAY=05 npm run day
+```
---
+++ day-template/solution.test.ts
@@ -0,0 +1,26 @@
+import { expect, test } from "vitest";
+// import { getLines, logSolution, readFile } from "../lib";
+
+test("Part 1", () => {
+  // TODO: template for day-xx
+  // const file = readFile("./day-xx/input.txt");
+  // const lines = getLines(file);
+  // const result = ;
+  // const expected = ;
+  // expect(result).toBe(expected);
+  // TODO: template for day-xx
+  // logSolution("xx", "1", expected);
+  expect(true).toBe(false);
+});
+
+test("Part 2", () => {
+  // TODO: template for day-xx
+  // const file = readFile("./day-xx/input.txt");
+  // const lines = getLines(file);
+  // const result = ;
+  // const expected = ;
+  // expect(result).toBe(expected);
+  // TODO: template for day-xx
+  // logSolution("xx", "2", expected);
+  expect(true).toBe(false);
+});
